24c3053d686aead74cdc9fb0de7da162252f8ee0
Yusuf Shehadeh <yusufshehadeh@gmail.com>
2022-10-30 21:29:53 +0100

    FINISH TASK 3

--- src/state/Elevator.java
+++ src/state/Elevator.java
@@ -1,23 +1,57 @@
 package state;
 
-public class Elevator{
+public class Elevator {
 
-    public static int current_Floor=5;
+    public static int current_Floor = 5;
     int dest_Floor;
 
     private Elv_States State = new Elv_States();
 
-    public Elevator (int dest_floor)
-    {
-        this.dest_Floor=dest_floor;
-        System.out.println("*** Destination floor is: "+ dest_Floor);
+    public Elevator(int dest_floor) {
+        this.dest_Floor = dest_floor;
+        System.out.println("*** Destination floor is: " + dest_Floor);
         arrive_atFloor();
 
 
     }
 
     private void arrive_atFloor() {
+        if (current_Floor > dest_Floor) {
+            State.Current_State = State.Moving_down;
+            System.out.println(State.Current_State);
+            while (current_Floor > dest_Floor) {
+                current_Floor--;
+                System.out.println(current_Floor);
+            }
+            State.Current_State = State.Idle;
+            System.out.println(announceFloor(current_Floor));
+        } else if (current_Floor < dest_Floor) {
+            State.Current_State = State.Moving_up;
+            System.out.println(State.Current_State);
+            while (current_Floor < dest_Floor) {
+                current_Floor++;
+                System.out.println(current_Floor);
+            }
+            State.Current_State = State.Idle;
+            System.out.println(announceFloor(current_Floor));
+        } else {
+            State.Current_State = State.Idle;
+            System.out.println(State.Current_State);
+            System.out.println(announceFloor(current_Floor));
+        }
+    }
 
+    private String announceFloor(int current_Floor){
+        switch(current_Floor){
+            case 1:
+                return "DING DING! Welcome to the 1st floor!";
+            case 2:
+                return "DING DING! Welcome to the 2nd floor!";
+            case 3:
+                return "DING DING! Welcome to the 3rd floor!";
+            default:
+                return "DING DING! Welcome to the "+current_Floor+"th floor!";
+        }
     }
 
 
--- src/state/Elv_States.java
+++ src/state/Elv_States.java
@@ -2,7 +2,10 @@ package state;
 
 public class Elv_States {
 
-    public  String Moving_up = "Moving_up";
+    public String Current_State;
+    public String Moving_up = "Moving_up";
+    public String Moving_down = "Moving_down";
+    public String Idle = "Idle";
 
 
 }
